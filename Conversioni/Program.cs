1f6841364fb32e770762a95c21c837e96e345874
Giulia Straccali <straccaligiulia@gmail.com>
2020-11-03 10:55:49 +0100

    Aggiungere i file di progetto.

---
+++ Conversioni/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Conversioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int divisore;
+            int resto;
+            string numeroConvertito = "";
+
+            Console.WriteLine("Inserisci un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Inserisci un divisore");
+            divisore = Convert.ToInt32(Console.ReadLine());
+
+            while (numero > 0)
+            {
+                resto = numero % divisore;
+                numero = numero / 2;
+                numeroConvertito = Convert.ToString(resto) + numeroConvertito;
+            }
+
+            Console.WriteLine($"{numeroConvertito}");
+            Console.ReadKey();
+        }
+    }
+}
